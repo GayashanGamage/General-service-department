367427dcfecb52c99911416b3a83b8b9874e7760
gayashan <gayashan.randimagamage@gmail.com>
2025-10-24 00:22:46 +0530

    DEBUG: change password validation logic and add self loging feature

--- Required Code - The Robot Driver/app.py
+++ Required Code - The Robot Driver/app.py
@@ -1,7 +1,9 @@
 from playwright.sync_api import sync_playwright
 import time
-# from playwright.async_api import async_playwright
-# from email_validator import validate_email, EmailNotValidError
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 class amazonProductsearch:
 
@@ -9,8 +11,8 @@ class amazonProductsearch:
         self.price = 0
         self.title = ''
         self.searchQuery = ''
-        self.userEmail = 'gayashan.randimagamage@gmail.com'
-        self.userPassword = '26efoi'
+        self.userEmail = ''
+        self.userPassword = ''
         self.availableBrowserList = []
 
         self.browser = ''
@@ -23,7 +25,7 @@ class amazonProductsearch:
         check what are the browsers available with the playwright environment
         return : none
         """
-        browser_name_list = ['chromium','firefox', 'webkit']
+        browser_name_list = ['firefox', 'chromium', 'webkit']
 
         print("checking available browsers ....")
         for item in browser_name_list:
@@ -41,6 +43,7 @@ class amazonProductsearch:
         else:
             self.launchBrowser()
 
+
     def launchBrowser(self):
         """
         launch the web browser + new tab with open amazon.com website on it
@@ -48,7 +51,6 @@ class amazonProductsearch:
         """
         try:
             with sync_playwright() as p:
-                print('this is try to execute')
                 for item in self.availableBrowserList:
                     self.browser = getattr(p, item) .launch()
                     self.context = self.browser.new_context()
@@ -57,20 +59,28 @@ class amazonProductsearch:
                     self.tab_one.goto('https://www.amazon.com/')
                     self.tab_one.wait_for_load_state('domcontentloaded')
                     
+                    # page before homepage with 'continue shopping' button
                     if self.tab_one.get_by_role("button", name="Continue shopping").is_visible():
                         self.tab_one.get_by_role("button", name="Continue shopping").click()
                         print(f'Amazon.com loaded in to {item} browser')
                         print(self.tab_one.title())
                         self.directToLogin()
                         break
-
+                    # this is for rare forntend
+                    elif self.tab_one.get_by_role("link", name="Your Account").is_visible():
+                        self.tab_one.get_by_role("link", name="Your Account").click()
+                        print(f'Amazon.com loaded in to {item} browser')
+                        print(self.tab_one.title())
+                        self.directToLogin()
+                        break
+                    # regular frontend
                     elif self.tab_one.title().strip() == 'Amazon.com. Spend less. Smile more.':
                         print(f'Amazon.com loaded in to {item} browser')
                         print(self.tab_one.title())
                         self.directToLogin()
                         break
         except: 
-            print('any browser cannot load amazon.com - check it manually from your browser.')
+            print('thanks for use this program... ')
 
     def directToLogin(self):
         """
@@ -94,6 +104,15 @@ class amazonProductsearch:
             print('Enter valied input.')
             self.chooseCredencialOptions()
     
+    def logInToSystem(self):
+        self.tab_one.get_by_role("textbox", name="Enter your mobile number or").fill(os.getenv('email'))
+        self.tab_one.get_by_role("button", name="Continue").click()
+        self.tab_one.wait_for_load_state('domcontentloaded')
+        self.tab_one.get_by_role("textbox", name="Password").fill(os.getenv('password'))
+        self.tab_one.get_by_role("button", name="Sign in").click()
+        self.tab_one.wait_for_load_state('domcontentloaded')
+        self.searchProduct()
+
     def askEmail(self):
         """
         if user decide to go with his credencials, ask what is the email / phone number that use for login to amazon.com
@@ -102,6 +121,7 @@ class amazonProductsearch:
         self.userEmail = input('Enter your amazon account email or mobile number : ')
         self.tab_one.get_by_role("textbox", name="Enter your mobile number or").fill(self.userEmail)
         self.tab_one.get_by_role("button", name="Continue").click()
+        time.sleep(2)
         self.tab_one.wait_for_load_state('domcontentloaded')
         self.checkAccountIsAvailable()
 
@@ -115,6 +135,7 @@ class amazonProductsearch:
         elif self.tab_one.get_by_role("heading", name="Looks like you're new to").is_visible():
             print('there is no amazon account under this email. enter correct one')
             self.tab_one.go_back()
+            self.tab_one.wait_for_load_state('domcontentloaded')
             self.askEmail()
 
     def checkUserPassword(self):
@@ -124,31 +145,36 @@ class amazonProductsearch:
         """
         self.userPassword = input('Enter your password : ')
         self.tab_one.get_by_role("textbox", name="Password").fill(self.userPassword)
-        self.tab_one.get_by_role("button", name="Sign in").click()
         self.tab_one.wait_for_load_state('domcontentloaded')
-        time.sleep(4)
-        if self.tab_one.get_by_role("heading", name="There was a problem").is_visible():
-            print('your password is incorrect. re try again')            
+        self.tab_one.get_by_role("button", name="Sign in").click()
+        self.tab_one.wait_for_load_state('networkidle')
+
+        current_url = self.tab_one.url
+        if 'amazon.com' in current_url and '/ap/signin' not in current_url:
+            print('Login successful! Now you can search products')
+            self.searchProduct()
+        elif self.tab_one.get_by_role("heading", name="There was a problem").is_visible():
+            print('Your password is incorrect. Please try again')            
             self.tab_one.go_back()
             self.tab_one.wait_for_load_state('domcontentloaded')
             self.checkUserPassword()
-        elif self.tab_one.title().strip() == 'Amazon.com. Spend less. Smile more.':
-            print('now you can search products')
-            self.searchProduct()
-
-
 
     def searchProduct(self):
         """
         check users preffered items price on amazon.com
         return : none
         """
-        self.searchQuery = input('what you want to search ? ')
         self.tab_one.wait_for_load_state('domcontentloaded')
-        self.tab_one.get_by_role("searchbox", name="Search Amazon").fill(self.searchQuery)
-        self.tab_one.get_by_role("button", name="Go", exact=True).click()
-        self.tab_one.wait_for_load_state('domcontentloaded')
-        self.showProductDetails()
+        self.searchQuery = input('what you want to search ? ( end this program -> exit() ) ')
+
+        if self.searchQuery.strip() == 'exit()':
+            self.exit()
+        else:
+            self.tab_one.wait_for_load_state('domcontentloaded')
+            self.tab_one.get_by_role("searchbox", name="Search Amazon").fill(self.searchQuery)
+            self.tab_one.get_by_role("button", name="Go", exact=True).click()
+            self.tab_one.wait_for_load_state('domcontentloaded')
+            self.showProductDetails()
 
 
     def showProductDetails(self):
@@ -159,12 +185,12 @@ class amazonProductsearch:
         self.tab_one.wait_for_load_state('domcontentloaded')
         self.tab_one.wait_for_selector('.s-result-item')
          
-        self.tab_one.wait_for_load_state('domcontentloaded')
-        product_prices = self.tab_one.locator('.a-size-base-plus').all()
-        for title in product_prices[:1]:
+        # self.tab_one.wait_for_load_state('domcontentloaded')
+        product_title = self.tab_one.locator('.a-size-base-plus.a-spacing-none.a-color-base.a-text-normal').all()
+        for title in product_title[:1]:
             print("Title:", title.text_content())
 
-        self.tab_one.wait_for_load_state('domcontentloaded')
+        # self.tab_one.wait_for_load_state('domcontentloaded')
         product_prices = self.tab_one.locator('.a-price .a-offscreen').all()
         for price in product_prices[:1]:
             print("Price:", price.text_content())
@@ -172,6 +198,3 @@ class amazonProductsearch:
         
 
 amazonProductsearch()
-
-
-
